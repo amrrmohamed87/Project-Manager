ee25ad1264173e54e089b946b4e1e0e160650b76
Amr <frank1276smith@gmail.com>
2023-12-24 21:59:31 +0200

    importing NewProject.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import NewProject from "./components/NewProject";
 import Siderbar from "./components/Sidebar";
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <Siderbar />
+      <NewProject />
     </main>
   );
 }
