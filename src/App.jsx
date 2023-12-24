3bd3d94f4347aaee69087014bb899c674c86965a
Amr <frank1276smith@gmail.com>
2023-12-25 00:26:33 +0200

    importing NoProjectSelected

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import Siderbar from "./components/Sidebar";
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Siderbar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
