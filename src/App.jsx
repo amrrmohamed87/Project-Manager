2e32af238b654b8769c946998c58b0eb128028f5
Amr <frank1276smith@gmail.com>
2023-12-25 10:24:43 +0200

    adding useState to manage states and switching between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,76 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Siderbar from "./components/Sidebar";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onSelect={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Siderbar />
-      <NoProjectSelected />
+      <Siderbar onSelect={handleStartAddProject} />
+      {content}
     </main>
   );
 }
 
 export default App;
+
+/* maxmillian solution of managing state to switch between components
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onSelect={handleStartAddProject} />
+  }
+*/
+
+/* My solution (it works but maxmillian wants to store projects and my way does not store projects)
+  const [projectSelected, setProjectSelected] = useState(false);
+
+  function handleSelect() {
+    setProjectSelected(true);
+  }
+
+  return (
+    {projectSelected ? (
+        <NewProject />
+      ) : (
+        <NoProjectSelected onSelect={handleSelect} />
+      )}
+  )
+
+*/
