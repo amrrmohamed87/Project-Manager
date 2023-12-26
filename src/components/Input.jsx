0955075dfcffbb6bafe6b39da900e1df53513a33
Amr <frank1276smith@gmail.com>
2023-12-26 13:01:04 +0200

    applying forwardRef to access values from another components

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,18 @@
-export default function Input({ label, textarea, ...props }) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
   return (
     <p className="flex flex-col my-4">
       <label className="font-bold text-sm uppercase text-stone-500">
         {label}
       </label>
       {textarea ? (
-        <textarea className="inputs" {...props} />
+        <textarea ref={ref} className="inputs" {...props} />
       ) : (
-        <input className="inputs" {...props} />
+        <input ref={ref} className="inputs" {...props} />
       )}
     </p>
   );
-}
+});
+
+export default Input;
