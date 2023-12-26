21511c18a4ecab92f66251b8ba5aff229e5519d2
Amr <frank1276smith@gmail.com>
2023-12-26 13:02:18 +0200

    Showing my projects in the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 
-export default function Siderbar({ onSelect }) {
+export default function Siderbar({ onSelect, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl  ">
       <h2 className=" mb-8 uppercase md:text-xl text-sky-200 font-bold mt-6">
@@ -9,7 +9,15 @@ export default function Siderbar({ onSelect }) {
       <div>
         <Button onClick={onSelect}>+ Add projects</Button>
       </div>
-      <ul>...</ul>
+      <ul className="mt-2">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
